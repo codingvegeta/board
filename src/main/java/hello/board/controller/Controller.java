8c057080c032afbb3ce97e020eb3dd4078a22edc
codingvegeta <hong090992@gmail.com>
2023-06-14 00:36:29 +0900

    23/6/14 뭔가 만드는중

--- src/main/java/hello/board/controller/Controller.java
+++ src/main/java/hello/board/controller/Controller.java
@@ -3,46 +3,57 @@ package hello.board.controller;
 import hello.board.response.Response;
 import hello.board.user.User;
 import hello.board.user.UserDTO;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.Session;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @org.springframework.stereotype.Controller
 public class Controller {
     @GetMapping("/")
-    public String index(Model model) {
-        return "index";
+    public ModelAndView index(ModelAndView modelAndView) {
+        log.info("로직이 실행되기 이전의 modelAndView = {}", modelAndView);
+        modelAndView.setViewName("/index");
+        User user = new User();
+        user.setNickName("Hello World");
+        modelAndView.addObject("user", user);
+        log.info("로직이 실행된 이후의 modelAndView = {}", modelAndView);
+        return modelAndView;
     }
 
     @GetMapping("/into")
-    public String into() {
+    public ModelAndView into(ModelAndView modelAndView) {
         log.info("get into");
-        return "/into";
+        log.info("로직이 실행되기 이전의 modelAndView = {}", modelAndView);
+        modelAndView.setViewName("/into");
+        log.info("로직이 실행된 이후의 modelAndView = {}", modelAndView);
+        return modelAndView;
     }
 
     @PostMapping("/into")
     @ResponseBody
-    public Object postInto(@RequestBody @Validated UserDTO userDTO, BindingResult result) {
+    public Response postInto(@RequestBody @Validated UserDTO userDTO, BindingResult result, ModelAndView modelAndView) {
+
         log.info("post into");
+        log.info("로직이 실행되기 이전의 modelAndView = {}", modelAndView);
+
         User user = new User();
         user.setUserName(userDTO.getUserName());
         user.setNickName(userDTO.getNickName());
 
         if (result.hasErrors()) {
+            log.info("로직이 실행된 이후의 modelAndView = {}", modelAndView);
             return Response.of(false, result.getAllErrors());
         }
 
-        return "/hello";
+        modelAndView.addObject("user", user);
+        log.info("로직이 실행된 이후의 modelAndView = {}", modelAndView);
+        return Response.of("/");
     }
+
 }
